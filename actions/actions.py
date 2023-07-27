ec2e7f43ac3408586de9a754b7627973232590eb
Amin Eshghi <amin.t.eshghi@gmail.com>
2023-07-27 08:00:36 -0700

    test dynamic form

--- actions/actions.py
+++ actions/actions.py
@@ -1,16 +1,42 @@
 from typing import Text, List, Any, Dict
 
-from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.events import EventType, SlotSet
 
+import logging
+logger = logging.getLogger(__name__)
 
-def clean_name(name):
-    return "".join([c for c in name if c.isalpha()])
+# def clean_name(name):
+#     return "".join([c for c in name if c.isalpha()])
 
 class ValidateNameForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_name_form"
+     
+    async def required_slots(
+        self,
+        domain_slots: List[Text],
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: "DomainDict",
+    ) -> List[Text]:
+        additional_slots = ["first_name", "last_name"]
+
+        logger.info('inside required_slots')
+        return additional_slots
+
+    async def extract_first_name(
+                self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+        ) -> Dict[Text, Any]:
+        logger.info('inside extract_first_name')
+
+        first_name = tracker.latest_message.get("text")
+
+        logger.info('extracted first name {}'.format(first_name))
+        return {"first_ame": first_name}
 
     def validate_first_name(
         self,
@@ -22,11 +48,18 @@ class ValidateNameForm(FormValidationAction):
         """Validate `first_name` value."""
 
         # If the name is super short, it might be wrong.
-        name = clean_name(slot_value)
-        if len(name) == 0:
-            dispatcher.utter_message(text="That must've been a typo.")
-            return {"first_name": None}
-        return {"first_name": name}
+        return {"first_name": slot_value}
+
+    async def extract_last_name(
+                self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+        ) -> Dict[Text, Any]:
+
+        logger.info('inside extract_last_name')
+
+        last_name = tracker.latest_message.get("text")
+
+        logger.info('extracted last name {}'.format(last_name))
+        return {"last_name": last_name}
 
     def validate_last_name(
         self,
@@ -37,14 +70,5 @@ class ValidateNameForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         """Validate `last_name` value."""
 
-        # If the name is super short, it might be wrong.
-        name = clean_name(slot_value)
-        if len(name) == 0:
-            dispatcher.utter_message(text="That must've been a typo.")
-            return {"last_name": None}
-        
-        first_name = tracker.get_slot("first_name")
-        if len(first_name) + len(name) < 3:
-            dispatcher.utter_message(text="That's a very short name. We fear a typo. Restarting!")
-            return {"first_name": None, "last_name": None}
-        return {"last_name": name}
+    
+        return {"last_name": slot_value}
